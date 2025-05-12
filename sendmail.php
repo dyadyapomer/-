61f26a9bea079d517a51f0a4a5f7ae3506a737cc
dyadyapomer <vova.kryt.pashynskyi@gmail.com>
2025-05-13 00:03:02 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,1 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $to = "smusroman@gmail.com";  // Сюди прийде лист
+  $subject = "Нове замовлення з сайту МилоМанія";
 
+  $message = "Обрані товари: " . $_POST['items'] . "\n"
+           . "Ім'я: " . $_POST['name'] . "\n"
+           . "Телефон: " . $_POST['phone'] . "\n"
+           . "Адреса доставки: " . $_POST['address'];
+
+  $headers = "From: no-reply@milomania.com.ua\r\n";
+  $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+
+  if (mail($to, $subject, $message, $headers)) {
+    echo "<h1>Дякуємо за замовлення!</h1>";
+  } else {
+    echo "<h1>Сталася помилка. Спробуйте ще раз.</h1>";
+  }
+}
+?>
